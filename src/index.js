243d2fa2bfc6b537b5969b58678240a5ed8298cd
Danilo Bezerra <danilobezerra970@gmail.com>
2022-03-29 14:23:06 -0300

    arquivos

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>teste</div>
   </React.StrictMode>,
   document.getElementById('root')
 );
