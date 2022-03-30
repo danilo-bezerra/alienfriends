789c21721d0709d7f1032ca16e360488c60a6906
Danilo Bezerra <danilobezerra970@gmail.com>
2022-03-30 15:19:18 -0300

    melhorias

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,7 @@
-import './Card.css'
-
 const Card = ({ id, name, email }) => {
     return (
         <div className='tc bg-light-blue pa3 grow dib shadow-5 ma2 br3 bw'>
-            <img src={`https://robohash.org/${id}?set=set2&size=200x200`} alt='robot'/>
+            <img src={`https://robohash.org/${id ** 5 + 2}?set=set2&size=200x200&bgset=bg1`} alt='robot' style={{borderRadius: '5px'}}/>
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
